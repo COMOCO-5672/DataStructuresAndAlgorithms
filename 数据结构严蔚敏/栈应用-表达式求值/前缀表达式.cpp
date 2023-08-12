fa6efb793fa69e2f98fc86e6ed2c2eeb86219874
“jiaopl” <jiaopl@bokecc.com>
2023-08-12 15:44:09 +0800

    add middle summtor

---
+++ 数据结构严蔚敏/栈应用-表达式求值/前缀表达式.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <stack>
+
+int precopt(const std::string &str)
+{
+    char a = '0';
+
+    std::stack<int>
+
+    return a - '0';
+}
+
+int main()
+{
+    std::string str = "-+1×+2345";
+
+    return 0;
+}
